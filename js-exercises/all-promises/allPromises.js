9a77af57ea6bf27a4bc93c603575e02ffcc96383
Syed Imran Abidi <imrans110@hotmail.com>
2020-06-17 14:35:01 +0530

    Rewrite promiseAll Solution

--- js-exercises/all-promises/allPromises.js
+++ js-exercises/all-promises/allPromises.js
@@ -1,10 +1,16 @@
-const allPromises = async (args) =>
-  args.reduce(
-    (accumulator, currrentPromise) =>
-      accumulator.then((results) =>
-        Promise.resolve(currrentPromise).then((result) => [...results, result])
-      ),
-    Promise.resolve([])
-  );
+const allPromises = (promiseIterable) =>
+  new Promise((resolve, reject) => {
+    const settledPromises = [];
+    for (const P of promiseIterable) {
+      Promise.resolve(P)
+        .then((res) => {
+          settledPromises.push(res);
+          if (settledPromises.length === promiseIterable.length) {
+            resolve(settledPromises);
+          }
+        })
+        .catch((e) => reject(new Error(e)));
+    }
+  });
 
 export { allPromises };
